9cf72a8143257f710c8caa070e13cd55eb575017
Asmaa Badry <asmaabadry969@gmail.com>
2023-03-28 15:37:37 +0200

    create hotel page

---
+++ src/pages/hotel/Hotel.jsx
@@ -0,0 +1,13 @@
+
+import './hotel.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLocationDot,
+} from "@fortawesome/free-solid-svg-icons";
+const Hotel = () => {
+  return <div>Hotel
+     <FontAwesomeIcon icon={faLocationDot} />
+  </div>;
+};
+
+export default Hotel;
