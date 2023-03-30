25be8a1e077b9e0e0bcb7c759a68b9ed5d17d057
monahewidy <monaahmedhewidy@gmail.com>
2023-03-30 16:28:23 +0200

    hotels & slider

--- src/pages/hotel/Hotel.jsx
+++ src/pages/hotel/Hotel.jsx
@@ -1,6 +1,6 @@
 import "./hotel.css";
-// import Navbar from "../../components/navbar/Navbar";
-// import Header from "../../components/header/Header";
+import Navbar from "../../components/navbar/Navbar";
+import Header from "../../components/header/Header";
 // import MailList from "../../components/mailList/MailList";
 // import Footer from "../../components/footer/Footer";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -56,8 +56,8 @@ const handleMove = (direction) => {
 
 return (
     <div>
-    {/* <Navbar />
-    <Header type="list" /> */}
+    <Navbar />
+    <Header type="list" />
     <div className="hotelContainer">
         {open && (
         <div className="slider">
